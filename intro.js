b924e0ef54a1acd7bafbd89f2380f953b8a8cee6
EricSchles <ericschles@gmail.com>
2014-11-15 10:43:10 -0800

    first commit

---
+++ intro.js
@@ -0,0 +1,24 @@
+//intergers - 4
+//floats - 4.0 or 9.246591
+//strings - "stuff"
+
+var x = 4+"stuff";
+
+//alert(x);
+
+var add = function(first,second) {
+  return first+second;
+};
+
+var multiple = function(third,fourth){
+  return third*fourth;
+};
+
+var greeting = function(name){
+  return "Hello "+name;
+};
+
+alert(greeting("Lassie"));
+
+//alert(multiple(add(4,5),17));
+
